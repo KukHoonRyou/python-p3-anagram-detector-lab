bc1d88733225da3a014c39ea677bbe9c4f9d5f2c
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-04-04 22:01:00 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__ (self, word):
+        self.word = word
+
+    def match(self, words):
+        result = []
+        sorted_word = sorted(self.word.lower())
+        
+        for candidate in words:
+            if self.word.lower() != candidate.lower() and sorted_word == sorted(candidate.lower()):
+                result.append(candidate)
+
+
+        return result
+
+
